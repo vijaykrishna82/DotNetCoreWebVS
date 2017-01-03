1e1fec9d8be944929114bef14581c644727c561c
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2017-01-02 22:17:46 -0500

    initial commit

---
+++ src/NetCoreVSWeb/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+
+namespace NetCoreVSWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseUrls("http://localhost:8081")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>();
+            
+             
+           var builtHost = host     .Build();
+           
+
+            builtHost.Run();
+        }
+    }
+}
