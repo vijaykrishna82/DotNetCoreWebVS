1babfaf02741172f8b4ba7ca0a149ccd5d9e3a6e
vijay.vepakomma@philips.com <vijay.vepakomma@philips.com>
2017-01-03 14:31:22 -0500

    Add Mvc and DeveloperExceptionPage

---
+++ src/NetCoreVSWeb/Controllers/MariaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreVSWeb.Controllers
+{
+    public class MariaController
+    {
+      //  [HttpGet("/")]
+        public string Index() => "Hello from MVC!";
+
+        //[HttpGet("/Maria")]
+        public string Maria() => "Hello from Maria!";
+    }
+}
--- src/NetCoreVSWeb/Startup.cs
+++ src/NetCoreVSWeb/Startup.cs
@@ -32,19 +32,47 @@ namespace NetCoreVSWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging();
-            services.AddDirectoryBrowser();
-            services.AddRouting();
+            //services.AddDirectoryBrowser();
+            //services.AddRouting();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IHostingEnvironment env)
         {
             loggerFactory.AddConsole();
+            //AddStaticServe(app);
+
+            //AddRouting(app);
+
+            //app.UseMvc();
+
+            app.UseDeveloperExceptionPage();
+            app.UseMvc(x => x.MapRoute(name: "default", template: "{controller=Maria}/{action=Index}/{id?}"));
+
+            
+
+            //default controller = home, default action = index, id is optional
+
+            app.Run(async (context) =>
+            {
+                await context.Response
+                .WriteAsync($"Hello Vijay! {env.EnvironmentName} {Configuration["message"]}" +
+                $" { SampleLibrary.Class1.Greeting()}");
+            });
+
+
+        }
+
+        private static void AddStaticServe(IApplicationBuilder app)
+        {
             //app.UseDirectoryBrowser();
             //app.UseFileServer();
             app.UseStaticFiles();
+        }
 
-
+        private static void AddRouting(IApplicationBuilder app)
+        {
             var routeBuilder = new RouteBuilder(app);
             routeBuilder.MapGet("route", x => x.Response.WriteAsync("Hello from Routing"));
             routeBuilder.MapGet("route2", x => x.Response.WriteAsync("Hello from Routing2"));
@@ -55,15 +83,6 @@ namespace NetCoreVSWeb
             var router = routeBuilder.Build();
 
             app.UseRouter(router);
-
-            app.Run(async (context) =>
-            {
-                await context.Response
-                .WriteAsync($"Hello Vijay! {env.EnvironmentName} {Configuration["message"]}" +
-                $" { SampleLibrary.Class1.Greeting()}");
-            });
-
-
         }
     }
 }
